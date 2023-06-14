e217cb09725d7c3cbbb63d9b1deb852c397bbbc4
codewizo <ongliberius@gmail.com>
2023-06-14 14:42:12 +0300

    Exercises for practice

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function (string, numTimes) {
+    let repeat = string.repeat(numTimes);
+    console.log(repeat);
 };
-
+repeatString('hey', 3);
 // Do not edit below this line
 module.exports = repeatString;
